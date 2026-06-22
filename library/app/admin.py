407374765f3b29c58426be0886fe80f348d650f3
Denys Levshun <djoni1vincent@djoni1vincent.local>
2026-06-23 00:48:11 +0200

    Add README and enhance admin with search, filters, and inlines

--- library/app/admin.py
+++ library/app/admin.py
@@ -1,3 +1,5 @@
+from re import search
+
 from django.contrib import admin
 
 from .models import Author, Book, Genre, Profile, Review
@@ -8,6 +10,18 @@ from .models import Author, Book, Genre, Profile, Review
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ["name", "isbn"]
+    search_fields = ["name", "authors__name", "isbn"]
+    list_filter = ["genres"]
+
+
+class BookInline(admin.TabularInline):
+    model = Book.authors.through
+    extra = 1 
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    inlines = [BookInline]
 
 
 @admin.register(Review)
@@ -15,6 +29,5 @@ class ReviewAdmin(admin.ModelAdmin):
     list_display = ["book", "user", "rating"]
 
 
-admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(Profile)
